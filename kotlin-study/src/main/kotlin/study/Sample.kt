c4a02ad79b4d09fa05b20a0a5671b59f408fb33e
cscs8 <42040439+cscs8@users.noreply.github.com>
2019-12-26 23:46:53 +0900

    Update Sample, SampleTest

--- kotlin-study/src/main/kotlin/study/Sample.kt
+++ kotlin-study/src/main/kotlin/study/Sample.kt
@@ -1,15 +1,18 @@
-fun contains(ints: List<Int>, predicate: (Int) -> Boolean): Boolean {
-    for (i in ints) {
-        if (predicate(i)) return true
+class Sample {
+    fun contains(ints: List<Int>, predicate: (Int) -> Boolean): Boolean {
+        for (i in ints) {
+            if (predicate(i)) return true
+        }
+        return false
     }
-    return false
+
+    fun isGreaterThan5(n: Int): Boolean = 5 < n
+
+    fun isMultipleOf3(n: Int): Boolean = n % 3 == 0
 }
 
-fun isGreaterThan5(n: Int): Boolean = 5 < n
-
-fun isMultipleOf3(n: Int): Boolean = n % 3 == 0
-
 fun main(args: Array<String>) {
-    println(contains(listOf(2, 4, 6), ::isGreaterThan5))
-    println(contains(listOf(7, 5, 4), ::isMultipleOf3))
+    val sample = Sample()
+    println(sample.contains(listOf(2, 4, 6), sample::isGreaterThan5))
+    println(sample.contains(listOf(7, 5, 4), sample::isMultipleOf3))
 }
